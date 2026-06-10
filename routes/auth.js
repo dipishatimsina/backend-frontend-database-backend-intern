ac725473559abc72abccb63cd0aa17062431bc62
dipishatimsina <dipishatimsina1@gmail.com>
2026-06-10 10:57:57 +0545

    Refactor auth routes: organize imports and add users route file

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import {login, register} from '../controllers/authController.js';
+import {
+    login,
+    logout,
+    refreshToken, 
+    register,
+} from '../controllers/authController.js';
 
 const router = express.Router();
 
