ea59b9cf72dd4079497fc66bc1649af87ad514d7
dipishatimsina <dipishatimsina1@gmail.com>
2026-06-12 12:58:44 +0545

    frontend Inatilization

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4 text-white">
+      <div>Navbar</div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const res = await axios.get("/api/auth/refresh", {
+                    withCredentials: true,
+                });
+                setAuth({ accessToken: res.data.accessToken, role: res.data.role });
+            } catch (error) {
+                setAuth(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        checkAuth();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ auth, loading, setAuth }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
+
+
+        
