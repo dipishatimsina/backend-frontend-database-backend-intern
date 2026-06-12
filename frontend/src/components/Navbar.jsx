6bc57cd989bc8a84b50de2c0bd795506265b450f
dipishatimsina <dipishatimsina1@gmail.com>
2026-06-12 15:07:33 +0545

    Fix typo in Navbar component button element and clean up formatting

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,36 +4,35 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const { auth } = useAuth();
 
-
   return (
     <nav className="bg-gray-800 p-4 text-white">
-      <div className="container mx-auto flex items-center justify-between"> 
+      <div className="container mx-auto flex items-center justify-between">
         <div>
           <Link to="/" className="text-white mr-4">
             Home
           </Link>
           <div>
-            {auth?.accessToken ? <>
-                <buton>
+            {auth?.accessToken ? (
+              <>
+                <button>
                   Logout
-                </buton>
-
-            </> :
-            <>
+                </button>
+              </>
+            ) : (
+              <>
                 <Link to="/Login" className="text-white mr-4">
                   Login
                 </Link>
                 <Link to="/Register" className="text-white mr-4">
                   Register
                 </Link>
-              }
+              </>
+            )}
           </div>
-          
         </div>
       </div>
     </nav>
   );
 };
-
 
 export default Navbar
