9b842788cf865a3f7718ed6ea68c1c1a3e002e77
dipishatimsina <dipishatimsina1@gmail.com>
2026-06-12 13:32:50 +0545

    Fix Navbar component layout and add authentication handling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,34 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const { auth } = useAuth();
+
+
   return (
     <nav className="bg-gray-800 p-4 text-white">
-      <div>
-        <Link to="/" className="text-white mr-4">
-          Home
-        </Link>
+      <div className="container mx-auto flex items-center justify-between"> 
+        <div>
+          <Link to="/" className="text-white mr-4">
+            Home
+          </Link>
+          <div>
+            {auth?.accessToken ? <>
+                <buton>
+                  Logout
+                </buton>
+
+            </> :
+            <>
+                <Link to="/Login" className="text-white mr-4">
+                  Login
+                </Link>
+                <Link to="/Register" className="text-white mr-4">
+                  Register
+                </Link>
+              }
+          </div>
+          
+        </div>
       </div>
     </nav>
   );
