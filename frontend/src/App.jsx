12aca03b5608d30c70a8cb4ff9d15b0037724624
dipishatimsina <dipishatimsina1@gmail.com>
2026-06-14 11:47:01 +0545

    Add routing for Login page in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Navbar />
+        <Routes>
+          <Route path="/login" elemeent={<Login />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   );
